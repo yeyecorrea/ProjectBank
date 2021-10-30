d9aabc1d30b1661a8ebc3833f847faa1b8325601
Yeferson Correa <yeyecorrea838@gmail.com>
2021-10-30 16:53:14 -0500

    descarga del paquete Newtonsoft.Json

--- User.cs
+++ User.cs
@@ -2,6 +2,19 @@ namespace ProjectBank
 {
     public class User
     {
-        
+        private string name { get; set; }
+        private string lastName { get; set; }
+        private int privateKey { get; set; }
+        public User(string name, string lastName, int privateKey)
+        {
+            this.name = name;
+            this.lastName = lastName;
+            this.privateKey = privateKey; 
+        }
+
+        public void CreateUser()
+        {
+
+        }
     }
 }
